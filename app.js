d62c444eff5f7f93e1ce029f0143ead2ef4c042e
Darren Tan <dtan0032@student.monash.edu>
2021-09-06 21:13:39 +1000

    done

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const actors = require('./routers/actor');
+const movies = require('./routers/movie');
+
+const app = express();
+
+app.listen(8080);
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect('mongodb://localhost:27017/movies', function (err) {
+    if (err) {
+        return console.log('Mongoose - connection error:', err);
+    }
+    console.log('Connect Successfully');
+
+});
+
+//Configuring Endpoints
+//Actor RESTFul endpoionts 
+app.get('/actors', actors.getAll);
+app.post('/actors', actors.createOne);
+app.get('/actors/:id', actors.getOne);
+app.put('/actors/:id', actors.updateOne);
+app.post('/actors/:id/movies', actors.addMovie); //adds actor into actors document, movie in body, actor id in params
+app.delete('/actors/:id', actors.deleteOne);
+app.delete('/actorsNmovies/:id', actors.deleteOnenActors);
+
+//Movie RESTFul  endpoints
+app.get('/movies', movies.getAll);
+app.post('/movies', movies.createOne);
+app.get('/movies/:id', movies.getOne);
+app.put('/movies/:id', movies.updateOne);
+app.delete('/movies/:id', movies.deleteOne);
